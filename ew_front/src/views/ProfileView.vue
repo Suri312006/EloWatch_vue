393b853e3da5e2ce8d89b5260da67e73a1fe8bef
Surendra Jammishetti <suri312006@gmail.com>
2023-09-15 21:28:01 -0700

    designing ProfileView.vue

--- ew_front/src/views/ProfileView.vue
+++ ew_front/src/views/ProfileView.vue
@@ -1,13 +1,39 @@
 
 
 <template>
-<h1 class="text-white ">
-  penis
-</h1>
+  <link href="https://fonts.googleapis.com/css2?family=Nova+Mono&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Nova+Mono&family=PT+Mono&display=swap" rel="stylesheet">
+
+  <div class="bg-black grid grid-cols-12">
+    <img
+        src="https://opgg-static.akamaized.net/meta/images/profile_icons/profileIcon4656.jpg?image=q_auto,f_webp,w_auto&v=1694664078578"
+        alt="penis"
+
+        class="rounded col-span 2"
+    >
+    <div class="flex flex-col col-span-4">
+      <h1 class="col-span-2 mt-4 ml-5 font-nova-pt-mono text-white text-5xl">SirYum</h1>
+      <h1 class="ml-8 mt-1 text-white">ladder rank<span class="text-blue-400"> 1 (top 100%)</span></h1>
+    </div>
+
+    
+  </div>
 </template>
 
 <style scoped>
+/* Add any additional styling you want for your search bar here */
+/* Add any additional styling you want for your toolbar here */
+.font-nova-mono {
+  font-family: 'Nova Mono', monospace;
+  /* You can adjust the font size, letter spacing, and other properties here */
+  letter-spacing: 0px;
+  text-shadow: 2px 2px 3px rgba(255, 255, 255, 0.5);
+}
+.font-nova-pt-mono{
 
+font-family: 'PT Mono', monospace;
+
+}
 </style>
 
 <script setup>
@@ -33,4 +59,6 @@ function getProfile(){
       })
 }
 
+</script>
+<script setup>
 </script>
